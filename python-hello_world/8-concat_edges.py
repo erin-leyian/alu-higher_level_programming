b24956f3fce3ef630c4c30e4dff57b17a3d02fa9
root <e.leyian@alustudent.com>
2025-06-24 13:00:34 -0700

    8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 
-print(" ".join(str.split()[5:7])+" " +str.split()[-4]+" " + str.split()[0])
+print(" ".join([str.split()[5]+" " +str.split()[6]+" " + str.split()[-4]+ " " + str.split()[0]]))
