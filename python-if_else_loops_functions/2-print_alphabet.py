4c1171787ef47fec5297b52eccdf2b7d83f69402
root <e.leyian@alustudent.com>
2025-06-25 15:11:43 -0700

    2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print("{}".format(''.join(chr(i) for i in range(ord('a'), ord('z') + 1))),end="")
+print(
+        "{}".format(''.join(chr(i)
+        for i in range(ord('a'), ord('z') + 1))) , end=""
+        )
