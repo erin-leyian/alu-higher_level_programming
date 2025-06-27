c67dc79edfbe246267bee11f61f6c74184117b0f
root <e.leyian@alustudent.com>
2025-06-27 15:03:03 -0700

    4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-
-
-def replace_in_list(my_list, idx, element):
-    if idx < 0:
+def new_in_list(my_list, idx, element):
+    if my_list is None:
+        return 1
+    elif idx < 0:
         return my_list
     elif idx > len(my_list) - 1:
         return my_list
-    my_list[idx] = element
-    return my_list
+    else:
+        c = list(my_list)
+        c[idx] = element
+    return c
