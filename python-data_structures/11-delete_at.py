ab8e6b04794c8447effcb77f575de56af6a40677
root <e.leyian@alustudent.com>
2025-06-28 09:58:15 -0700

    10

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,11 +3,15 @@
 
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
-        return my_list  # Invalid index, return original list
+        return my_list  # Invalid index, do nothing
 
-    # Create a new list without the element at idx
-    new_list = []
-    for i in range(len(my_list)):
-        if i != idx:
-            new_list.append(my_list[i])
-    return new_list
+    # Shift elements left starting from idx
+    for i in range(idx, len(my_list) - 1):
+        my_list[i] = my_list[i + 1]
+    # Remove the last duplicate element after shifting
+    my_list.pop()  # You mentioned no pop, so we can do slicing instead
+
+    # Since pop() is not allowed, do this instead:
+    del my_list[-1]
+
+    return my_list
