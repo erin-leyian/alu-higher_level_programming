a389d449b2dfa2b31c20a1605b5814bbb0ea6f0b
root <e.leyian@alustudent.com>
2025-07-02 05:59:24 -0700

    1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 
 
 def search_replace(my_list, search, replace):
-    return[ 
+    return[
             replace if search_replace == search else search_replace
             for search_replace in my_list
             ]
