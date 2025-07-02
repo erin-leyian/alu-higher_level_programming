51222b795f1d68cf8056544a9712320129e1d73c
root <e.leyian@alustudent.com>
2025-07-02 04:55:36 -0700

    0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
+
 def square_matrix_simple(matrix=[]):
-   return [[element ** 2 for element in row] for row in matrix]
+     return [[element ** 2 for element in row] for row in matrix]
