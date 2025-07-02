4c9776378c0a775f22fdb7718ca915bb97a4eb61
root <e.leyian@alustudent.com>
2025-07-02 12:02:30 -0700

    4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-     result_list = []
+    result_list = []
 
     for i in range(list_length):
         try:
@@ -18,4 +18,5 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         finally:
             result_list.append(result)
+
     return result_list
