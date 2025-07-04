f647248211a684ea0ab59f7c2bded54a6a13b5fe
root <e.leyian@alustudent.com>
2025-07-04 09:36:21 -0700

    1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 class Square:
      """
-    Represents a square.
-
-    Attributes:
-        __size (int): The size of the square (private).
-    """
+     Represents a square.
+ 
+     Attributes:
+         __size (int): The size of the square (private).
+     """
     def __init__(self,size):
         """
         Initializes a new Square instance.
