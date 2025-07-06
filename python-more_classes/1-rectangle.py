5f79f4e7a944c936b97ac5547209b06042eb19e0
root <e.leyian@alustudent.com>
2025-07-06 08:15:40 -0700

    1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module defines a Rectangle class with width and height attributes,
+including property getters and setters with type and value validation.
+"""
 class Rectangle:
     """Defines a rectangle with width and height."""
     def __init__(self, width=0,height=0):
